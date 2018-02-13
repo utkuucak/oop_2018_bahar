e0354eaea01b7d370407c36fca9c2fa3a1b5c36a
utkuu1349 <utkuu1349@gmail.com>
2018-02-13 10:47:23 +0300

    Hello World
    Started working on a basic console application.

---
+++ week_ii/firstWeek/firstWeek/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace firstWeek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello, World!");
+
+            //int a = 5;
+            //Console.WriteLine(a);
+
+            //int input = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine(input);
+
+            //double num = 5.145;
+            //string text = "Object Oriented Programming";
+
+            //Console.WriteLine(text);
+
+            string name = Console.ReadLine();
+            Console.WriteLine("Entered name is " + name + ".");
+
+            Console.ReadKey();
+        }
+    }
+}
